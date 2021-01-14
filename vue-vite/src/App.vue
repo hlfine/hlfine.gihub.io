c3bdb09c26cdc7e212fa32770f195a347c600c42
hanl <hanlei0706@rayootech.com>
2021-01-14 14:17:49 +0800

    init

--- vue-vite/src/App.vue
+++ vue-vite/src/App.vue
@@ -8,7 +8,7 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import NewCOmponent from './components/NewCOmponent'
+import NewCOmponent from './components/NewCOmponent.vue'
 
 export default {
   name: 'App',
