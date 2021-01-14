67c5c47eba4902b1528c2fdc4b74bdced1fd6562
hanl <hanlei0706@rayootech.com>
2021-01-14 14:16:23 +0800

    init

--- vue-vite/src/App.vue
+++ vue-vite/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Vue 3.0 + Vite" />
+    <NewCOmponent />
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NewCOmponent from './components/NewCOmponent'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NewCOmponent
   }
 }
 </script>
---
+++ vue-vite/src/components/NewCOmponent.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Description: 
+ * @Autor: hanl
+ * @Date: 2021-01-14 14:13:36
+ * @LastEditors: hanl
+ * @LastEditTime: 2021-01-14 14:16:04
+-->
+
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: 'NewComponent',
+  props: {
+    msg: {
+      type: String,
+      default: 'NewComponent'
+    }
+  },
+  data() {
+    return {};
+  },
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
